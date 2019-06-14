87b3aa9963d9f54ef90b70a3257744a2a72dfac5
Michal <michalszczep>
2019-06-14 22:45:56 +0200

    guava changed to commons due to unstable mark on Hashing from guava

--- src/main/java/com/msjconsulting/userstorage/utils/security/SHA256Encoder.java
+++ src/main/java/com/msjconsulting/userstorage/utils/security/SHA256Encoder.java
@@ -1,15 +1,13 @@
 package com.msjconsulting.userstorage.utils.security;
 
 
-import java.nio.charset.StandardCharsets;
-
-import com.google.common.hash.Hashing;
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class SHA256Encoder {
     public static String encodeUserNameAndPassword(String name, String password) {
         if (name == null || password == null || name.isEmpty() || password.isEmpty()) {
             throw new IllegalArgumentException("name and password have to be provided and can't be empty");
         }
-        return Hashing.sha256().hashString(name + password, StandardCharsets.UTF_8).toString();
+        return DigestUtils.sha256Hex(name + password);
     }
 }
